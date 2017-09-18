0ed09471321fc19968fb86acbf2c53864d53241a
Kazuyoshi Kawakami <saruyama.monki@gmail.com>
2017-09-18 09:16:42 +0900

    Bug fix

--- demo/server/index.php
+++ demo/server/index.php
@@ -5,7 +5,6 @@ date_default_timezone_set('Asia/Tokyo');
 if (isset($_SERVER['HTTP_ORIGIN'])) {
     header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
     header('Access-Control-Allow-Credentials: true');
-    header('Access-Control-Max-Age: 86400');
 }
 
 if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
